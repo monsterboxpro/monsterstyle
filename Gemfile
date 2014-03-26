abf71995864a6afe0b1ce1ce2abf403e86e4c3b0
Andrew Brown <andrew@monsterboxpro.com>
2014-03-26 12:06:37 -0300

    skeletion app

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
+
+gem 'middleman', '~>3.2.0'
+gem 'middleman-blog'
+gem 'middleman-deploy'
+gem 'middleman-livereload', '~> 3.1.0'
+gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
+gem 'susy'
---
+++ config.rb
@@ -0,0 +1,29 @@
+Time.zone = 'Eastern Time (US & Canada)'
+
+set :css_dir   , 'stylesheets'
+set :js_dir    , 'javascripts'
+set :images_dir, 'images'
+set :layout_dir, 'layouts'
+
+activate :livereload
+activate :directory_indexes
+
+activate :deploy do |deploy|
+  deploy.method   = :ftp
+  deploy.host     = 'ftp.monsterboxpro.com'
+  deploy.path     = '/public_html'
+  deploy.user     = 'monsterb'
+  deploy.password = 'scroll_SMB1'
+end
+
+activate :blog do |blog|
+  blog.prefix = 'blog'
+end
+
+configure :build do
+  activate :blog
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+end
